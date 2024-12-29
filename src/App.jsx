a11329fa0a4b0ad8b3b38bb7eb86041e191bfce0
aaaziim <azim210215@gmail.com>
2024-12-30 05:43:03 +0600

    Data Sent to server

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   
+
+  const handleAdduser = event =>{
+    event.preventDefault();
+    const name = event.target[0].value;
+    const email = event.target[1].value;
+    const user = {
+      name,
+      email,
+    }
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(response=>response.json())
+    .then(data =>{
+      console.log(data)
+    })
+    event.target.reset();
+  }
 
   return (
     <>
       <div>
       
         <h1>Simple Crud</h1>
+
+
+        <form onSubmit={handleAdduser}>
+          <input type="text" placeholder="Enter your name" />
+          <input type="email" placeholder="Enter your Email" />
+          <button type="submit">Add User</button>
+        </form>
       </div>
     
     </>
